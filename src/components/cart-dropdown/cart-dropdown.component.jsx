536cb0791f17589b984085df35519a72e55fc9a1
gabypega1997 <gabypega1997@gmail.com>
2023-02-12 21:33:09 +0100

    make a toggle cart item by myself

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,23 @@
+import { Fragment, useContext } from "react";
+
 import Button from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+    const { showDropdown } = useContext(CartContext);
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items"></div>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
+        <Fragment>
+            {showDropdown ? (
+                <div className="cart-dropdown-container">
+                    <div className="cart-items">
+                    
+                    </div>
+                    <Button>GO TO CHECKOUT</Button>
+                </div>
+            ) : null}
+        </Fragment>
     );
 };
 
---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext({
+    showDropdown: false,
+    setShowDropdown: () => null,
+});
+
+export const CartProvider = ({ children }) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const value = { showDropdown, setShowDropdown };
+    return (
+        <CartContext.Provider value={value}>{children}</CartContext.Provider>
+    );
+};
